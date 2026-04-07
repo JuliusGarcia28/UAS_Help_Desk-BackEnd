0b8fe001041cacad49e8124319e97fcb73112f35
Julian Garcia <julianjaviergarciaalvarez@gmail.com>
2026-04-07 15:52:41 -0700

    aumento de tiempo de vida jwt

--- config/settings.py
+++ config/settings.py
@@ -74,7 +74,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
